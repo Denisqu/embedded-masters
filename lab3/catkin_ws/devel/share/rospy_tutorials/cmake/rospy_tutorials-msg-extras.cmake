4b4f61f837ec5f1e621f4f99044abafc0762c2ff
Denisque <denis2000.kuzik@gmail.com>
2024-12-17 23:15:25 +0300

    удалил лишнюю папку

--- lab3/catkin_ws/devel/share/rospy_tutorials/cmake/rospy_tutorials-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(rospy_tutorials_MESSAGE_FILES "/home/student/myprojects/catkin_ws/src/ros_tutorials-melodic-devel/rospy_tutorials/msg/Floats.msg;/home/student/myprojects/catkin_ws/src/ros_tutorials-melodic-devel/rospy_tutorials/msg/HeaderString.msg")
-set(rospy_tutorials_SERVICE_FILES "/home/student/myprojects/catkin_ws/src/ros_tutorials-melodic-devel/rospy_tutorials/srv/AddTwoInts.srv;/home/student/myprojects/catkin_ws/src/ros_tutorials-melodic-devel/rospy_tutorials/srv/BadTwoInts.srv")
--- lab3/catkin_ws/devel/share/rospy_tutorials/cmake/rospy_tutorials-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(rospy_tutorials_MSG_INCLUDE_DIRS "/home/student/myprojects/catkin_ws/src/ros_tutorials-melodic-devel/rospy_tutorials/msg")
-set(rospy_tutorials_MSG_DEPENDENCIES std_msgs)
