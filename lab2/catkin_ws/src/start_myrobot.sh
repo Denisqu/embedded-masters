325c6070d26906b2024602e75b3c6c3a61b69550
Denisque <denis2000.kuzik@gmail.com>
2024-12-17 19:21:27 +0300

    lab 2

---
+++ lab2/catkin_ws/src/start_myrobot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#gnome-terminal -- bash -c "source ../devel/setup.sh; roscore; exec bash"
+
+gnome-terminal -- bash -c "source ../devel/setup.sh; roslaunch myrobot_control control.launch; exec bash"
+
+sleep 5
+
+gnome-terminal -- bash -c "source ../devel/setup.sh; roslaunch myrobot_simulator gazebo_testwalls.launch; exec bash"
+
+sleep 5
+
+gnome-terminal -- bash -c "source ../devel/setup.sh; roslaunch myrobot_description rviz.launch; exec bash"
+
+sleep 5
+
+gnome-terminal -- bash -c "source ../devel/setup.sh; rqt; exec bash"
+
+sleep 5
