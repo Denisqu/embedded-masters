1671cf0e2aee74907003ff18c42fe36668129445
Denisque <denis2000.kuzik@gmail.com>
2024-12-17 23:11:59 +0300

    lab3 WASD movement

---
+++ lab3/catkin_ws/devel/share/gennodejs/ros/turtlesim/srv/_index.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+let TeleportAbsolute = require('./TeleportAbsolute.js')
+let Kill = require('./Kill.js')
+let SetPen = require('./SetPen.js')
+let Spawn = require('./Spawn.js')
+let TeleportRelative = require('./TeleportRelative.js')
+
+module.exports = {
+  TeleportAbsolute: TeleportAbsolute,
+  Kill: Kill,
+  SetPen: SetPen,
+  Spawn: Spawn,
+  TeleportRelative: TeleportRelative,
+};
---
+++ lab3/catkin_ws/devel/share/roscpp_tutorials/cmake/roscpp_tutorials-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(roscpp_tutorials_MESSAGE_FILES "")
+set(roscpp_tutorials_SERVICE_FILES "/home/student/myprojects/catkin_ws/src/ros_tutorials-melodic-devel/roscpp_tutorials/srv/TwoInts.srv")
